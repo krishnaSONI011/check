3dea3728f3ea80419114a4d24a9982e4c4051fc0
marketingMavricks <marketingmavricksagency@gmail.com>
2025-07-29 10:08:37 +0530

    fix

--- components/MainCarousel.tsx
+++ components/MainCarousel.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React from "react";
-import Slider from "react-slick";
+import  ReactSlick from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import { Hero, heroes } from "./Hero";
@@ -21,6 +21,7 @@ export default function MainCarousel() {
     nextArrow: <IoIosArrowForward color="white" />,
     prevArrow: <IoIosArrowBack color="white" />,
   };
+  const Slider = ReactSlick as unknown as React.FC<any>;
   return (
     <div>
       <Slider {...settings} arrows={true}>
